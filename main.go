6410beb922bd637c23296e631aad26e808086b9e
MS05-Voyager <minudasaswitha05@gmail.com>
2025-05-07 00:05:26 +0530

    remove the unnecessary data

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
+//CREATING THE FILE LOADER
+//THIS WILL MAKE A SLICE WITH THE DATA EXTRACTED FROM THE CSV FILE
+
 package main
 
 import (
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	"strconv"
-	
 )
 
 //struct to group the reading values
@@ -24,7 +27,7 @@ func Load(path string) ([]stock, error) {
 		fmt.Println("Error: ", err)
 		return nil, err //exit the main function
 	}
-	defer f.Close() //close the files before the functions call
+	defer f.Close() //close the file as soon as the file open(successfully or with error) and make sure it close before the function end 
 	r := csv.NewReader(f)
 	rows, err := r.ReadAll()
 
@@ -68,6 +71,15 @@ func main() {
 	if err != nil {
 		return
 	}
+
+	fmt.Println(stocks)
+	fmt.Println()
+	//WE ONLY CONSIDER THE VALUES WITH THE GAP >= 10%(0.1)
+
+
+	stocks = slices.DeleteFunc(stocks, func(s stock) bool {
+		return math.Abs(s.Gap) < .1
+	})
 
 	fmt.Println(stocks)
 
